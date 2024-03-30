4dd54ae6e7b6b0eb91f3cb8ecbde4f86265c6e8f
Ankit Kokane <ankitkokane90@gmail.com>
2024-03-30 12:19:51 +0000

    minor changes

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,14 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 echo "Bars launched..."
